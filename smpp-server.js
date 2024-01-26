e407ee8f1f3844241c5142173e63ebadb3a87830
Issa Sukatu Abdullahi <sukaissa@gmail.com>
2024-01-26 06:04:59 +0000

    Updated db cred

--- smpp-server.js
+++ smpp-server.js
@@ -5,8 +5,8 @@ const db = mysql.createConnection({
     host: '127.0.0.1',
     port: 3306,
     user: 'root',
-    password: 'root',
-    database: 'xsenders'
+    password: 'Fidelity@2023',
+    database: 'smppserver'
 });
 
 const server = smpp.createServer({
